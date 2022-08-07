8da951f1ed3d00806ea385de55f6ff1e14ea1bba
LucioPimenta <luciosystem1@hotmail.com>
2022-08-07 01:46:41 -0300

    Refatoração no projeto ConstEnumerados

--- Consts/Constantes.cs
+++ Consts/Constantes.cs
@@ -1,18 +1,18 @@
 ﻿namespace ConstsEnumerados
 {
     public class Constantes
-  {
-    //Operação a ser executada pelo formulario
-    public const string Inserir = "Inserir";
-    public const string Alterar = "Alterar";
-    public const string Excluir = "Excluir";
+    {
+        //Operação a ser executada pelo formulario
+        public const string Inserir = "Inserir";
+        public const string Alterar = "Alterar";
+        public const string Excluir = "Excluir";
 
-    //Contantes de localização de CEP
-    public const string CepNaoEncontrado = "0";
-    public const string CepEncontrado = "1";
+        //Contantes de localização de CEP
+        public const string CepNaoEncontrado = "0";
+        public const string CepEncontrado = "1";
 
-    //Tabela de progresso para localização/configuração do bancos de dados
-    public const int metadeProgresso = 50;
-    public const int totalProgresso = 100;
-  }
+        //Tabela de progresso para localização/configuração do bancos de dados
+        public const int metadeProgresso = 50;
+        public const int totalProgresso = 100;
+    }
 }
--- Consts/Enumerados.cs
+++ Consts/Enumerados.cs
@@ -1,34 +1,34 @@
 ﻿namespace ConstsEnumerados
 {
     public class Enumerados
-  {
-    public enum OperacaoFormulario
     {
-      Inserir_Localizar = 1,
-      Salvar_Cancelar = 2,
-      Alterar_Excluir_Cancelar = 3
-    }
+        public enum OperacaoFormulario
+        {
+            Inserir_Localizar = 1,
+            Salvar_Cancelar = 2,
+            Alterar_Excluir_Cancelar = 3
+        }
 
-    public enum TipoClienteFornecedor
-    {
-      Pessoa_Fisica = 0,
-      Pessoa_Juridica = 1
-    }
-    public enum TipoPesquisa
-    {
-      CPF = 0,
-      CPNJ = 1,
-      NOME = 2
+        public enum TipoClienteFornecedor
+        {
+            Pessoa_Fisica = 0,
+            Pessoa_Juridica = 1
+        }
+        public enum TipoPesquisa
+        {
+            CPF = 0,
+            CPNJ = 1,
+            NOME = 2
 
-    }
+        }
 
-    public enum CampoParaFormatacao
-    {
-      CPF = 0,
-      CNPJ = 1,
-      CEP = 2,
-      TELEFONE,
-      CELULAR
+        public enum CampoParaFormatacao
+        {
+            CPF = 0,
+            CNPJ = 1,
+            CEP = 2,
+            TELEFONE,
+            CELULAR
+        }
     }
-  }
 }
